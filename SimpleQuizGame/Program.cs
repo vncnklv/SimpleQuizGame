e04d338e42313054a3a27ec4701c055c72f2595c
Ventsislav Nikolov <Ventsislav Nikolov@DESKTOP-I94QVEE>
2024-09-30 20:42:09 +0300

    Add project files.

---
+++ SimpleQuizGame/Program.cs
@@ -0,0 +1,8 @@
+﻿// This is a SimpleQuizGame to play while you are bored or you can't sleep.
+// The rules are simple:
+//  - There is only one correct answer.
+//  - Every game is composed of 10 random pre-generated questions
+//  - To win a game you must have no more than 3 wrong answers
+
+// Menu
+// 
